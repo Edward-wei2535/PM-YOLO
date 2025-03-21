b916b5c943951d0ecea0ef01264e1a774258dc48
Edward-wei2535 <66683523+Edward-wei2535@users.noreply.github.com>
2025-03-21 15:50:35 +0800

    Create PM-YOLO.py

---
+++ PM-YOLO..PY
@@ -0,0 +1,119 @@
+
+
+
+
+#BELOW IS ADOWN
+class ADown(nn.Module):
+    def __init__(self, c1, c2):  # ch_in, ch_out, shortcut, kernels, groups, expand
+        super().__init__()
+        self.c = c2 // 2
+        self.cv1 = Focus(c1 // 2, self.c)
+        self.cv2 = Conv(c1 // 2, self.c, 1, 1, 0)
+
+    def forward(self, x):
+        x = torch.nn.functional.avg_pool2d(x, 1, 1, 0, False, True)
+        x1, x2 = x.chunk(2, 1)
+        x1 = self.cv1(x1)
+        x2 = torch.nn.functional.max_pool2d(x2, 3, 2, 1)
+        x2 = self.cv2(x2)
+        return torch.cat((x1, x2), 1)
+#BELOW IS CSFCN
+class SFC_G2(nn.Module):
+    def __init__(self, inc):
+        super(SFC_G2, self).__init__()
+        hidc = inc[0]
+
+        self.groups = 2
+        self.conv_8 = Conv(inc[0], hidc, 3)
+        self.conv_32 = Conv(inc[1], hidc, 3)
+
+        self.conv_offset = nn.Sequential(
+            Conv(hidc * 2, 64),
+            nn.Conv2d(64, self.groups * 4 + 2, kernel_size=3, padding=1, bias=False)
+        )
+
+        self.keras_init_weight()
+        self.conv_offset[1].weight.data.zero_()
+
+class CFC_CRB(nn.Module):
+    def __init__(self, in_channels=512, grids=(6, 3, 2, 1)): # 先ce后ffm
+        super(CFC_CRB, self).__init__()
+        self.grids = grids
+        inter_channels = in_channels // 2
+        self.inter_channels = inter_channels
+
+        self.reduce_channel = Conv(in_channels, inter_channels, 3)
+        self.query_conv = nn.Conv2d(in_channels=inter_channels, out_channels=32, kernel_size=1)
+        self.key_conv = nn.Conv1d(in_channels=inter_channels, out_channels=32, kernel_size=1)
+        self.value_conv = nn.Conv1d(in_channels=inter_channels, out_channels=self.inter_channels, kernel_size=1)
+        self.key_channels = 32
+
+        self.value_psp = PSPModule(grids, inter_channels)
+        self.key_psp = PSPModule(grids, inter_channels)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        self.local_attention = LocalAttenModule(inter_channels,inter_channels//8)
+        self.keras_init_weight()
+# BELOW IS STARBLOCK-CAA
+class Star_Block(nn.Module):
+    def __init__(self, dim, mlp_ratio=3, drop_path=0.):
+        super().__init__()
+        self.dwconv = Conv(dim, dim, 7, g=dim, act=False)
+        self.f1 = nn.Conv2d(dim, mlp_ratio * dim, 1)
+        self.f2 = nn.Conv2d(dim, mlp_ratio * dim, 1)
+        self.g = Conv(mlp_ratio * dim, dim, 1, act=False)
+        self.dwconv2 = nn.Conv2d(dim, dim, 7, 1, (7 - 1) // 2, groups=dim)
+        self.act = nn.ReLU6()
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x1, x2 = self.f1(x), self.f2(x)
+        x = self.act(x1) * x2
+        x = self.dwconv2(self.g(x))
+        x = input + self.drop_path(x)
+        return x
+
+
+class Star_Block_CAA(Star_Block):
+    def __init__(self, dim, mlp_ratio=3, drop_path=0):
+        super().__init__(dim, mlp_ratio, drop_path)
+
+        self.attention = CAA(mlp_ratio * dim)
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x1, x2 = self.f1(x), self.f2(x)
+        x = self.act(x1) * x2
+        x = self.dwconv2(self.g(self.attention(x)))
+        x = input + self.drop_path(x)
+        return x
+
+
+class C3_Star(C3):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(Star_Block(c_) for _ in range(n)))
+
+
+class C2f_Star(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(Star_Block(self.c) for _ in range(n))
+
+
+class C3_Star_CAA(C3):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(Star_Block_CAA(c_) for _ in range(n)))
+
+
+class C2f_Star_CAA(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(Star_Block_CAA(self.c) for _ in range(n))
